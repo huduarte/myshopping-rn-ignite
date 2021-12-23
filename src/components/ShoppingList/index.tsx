b129be2046530b9757f341371abe33fb276395a5
Hudson <hudson.neto@sinqia.com.br>
2021-12-23 14:09:52 -0300

    feat: criando e listando documentos

--- src/components/ShoppingList/index.tsx
+++ src/components/ShoppingList/index.tsx
@@ -1,18 +1,49 @@
-import React, { useState } from 'react';
-import { FlatList } from 'react-native';
+import React, { useState } from "react";
+import { FlatList } from "react-native";
 
-import { styles } from './styles';
-import { Product, ProductProps } from '../Product';
+import firestore from "@react-native-firebase/firestore";
+import { styles } from "./styles";
+import { Product, ProductProps } from "../Product";
 
-import { shoppingListExample } from '../../utils/shopping.list.data';
+import { useEffect } from "react";
 
 export function ShoppingList() {
-  const [products, setProducts] = useState<ProductProps[]>(shoppingListExample);
+  const [products, setProducts] = useState<ProductProps[]>([]);
+
+  useEffect(() => {
+    firestore()
+      .collection("products")
+      .get()
+      .then((response) => {
+        const data = response.docs.map((doc) => {
+          return {
+            id: doc.id,
+            ...doc.data(),
+          };
+        }) as ProductProps[];
+
+        setProducts(data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  // useEffect(() => {
+  //   firestore()
+  //     .collection("products")
+  //     .doc("my-custom-id")
+  //     .get()
+  //     .then((response) =>
+  //       console.log({
+  //         ...response.data(),
+  //         id: response.id,
+  //       })
+  //     );
+  // }, []);
 
   return (
     <FlatList
-      data={shoppingListExample}
-      keyExtractor={item => item.id}
+      data={products}
+      keyExtractor={(item) => item.id}
       renderItem={({ item }) => <Product data={item} />}
       showsVerticalScrollIndicator={false}
       style={styles.list}
